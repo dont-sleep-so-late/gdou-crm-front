d3456f7fbc429e508b0b8350b3ca8d4136cdb0b0
dont-sleep-so-late <Imtiancai233@qq.com>
2024-07-11 00:40:51 +0800

    fix: 配置请求路径

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,11 @@ module.exports = defineConfig({
         changeOrigin: true,
         pathRewrite: { "^/app": "/ship" }, //代理服务器将请求地址转给真实服务器时会将 /api1 去掉
       },
+      "/": {
+        target: "http://localhost:8090", // 代理目标的基础路径
+        changeOrigin: true,
+        pathRewrite: { "^/": "/" }, //代理服务器将请求地址转给真实服务器时会将 /api1 去掉
+      },
     },
   },
 });
