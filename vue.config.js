af9a47756df8a383914968061ab71ca43d038100
dont-sleep-so-late <Imtiancai233@qq.com>
2024-07-11 00:48:48 +0800

    fix: 请求路径

--- vue.config.js
+++ vue.config.js
@@ -4,15 +4,14 @@ module.exports = defineConfig({
   lintOnSave: false,
   devServer: {
     proxy: {
+      "": {
+        target: "http://localhost:8090", // 代理目标的基础路径
+        changeOrigin: true,
+      },
       "/app": {
         target: "http://localhost:8090", // 代理目标的基础路径
         changeOrigin: true,
-        pathRewrite: { "^/app": "/ship" }, //代理服务器将请求地址转给真实服务器时会将 /api1 去掉
-      },
-      "/": {
-        target: "http://localhost:8090", // 代理目标的基础路径
-        changeOrigin: true,
-        pathRewrite: { "^/": "/" }, //代理服务器将请求地址转给真实服务器时会将 /api1 去掉
+        pathRewrite: { "^/app": "" }, //代理服务器将请求地址转给真实服务器时会将 /api1 去掉
       },
     },
   },
