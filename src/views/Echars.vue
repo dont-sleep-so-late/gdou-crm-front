99f0a9143427985d036e4048f6272dd3d594a57f
dont-sleep-so-late <Imtiancai233@qq.com>
2024-07-11 09:42:37 +0800

    feat: 饼图

--- src/views/Echars.vue
+++ src/views/Echars.vue
@@ -20,15 +20,15 @@
 			<div ref="echartsContainer3" style="width: 600px; height: 400px;"></div>
 		</div>
 
-   <!--部门-->
-    <div class="container" style="width: 600px; height: 400px;" id="box4">
-      <div id="echartsContainer4">
-        <el-select v-model="shipId" placeholder="请选择船舶" @input="getDeptCount(shipId)">
-          <el-option v-for="(item) in shipArray" :label="item.name" :value="item.id"></el-option>
-        </el-select>
-      </div>
-      <div ref="echartsContainer4" style="width: 600px; height: 400px;"></div>
-    </div>
+		<!--部门-->
+		<div class="container" style="width: 600px; height: 400px;" id="box4">
+			<div id="echartsContainer4">
+				<el-select v-model="shipId" placeholder="请选择船舶" @input="getDeptCount(shipId)">
+					<el-option v-for="(item) in shipArray" :label="item.name" :value="item.id"></el-option>
+				</el-select>
+			</div>
+			<div ref="echartsContainer4" style="width: 600px; height: 400px;"></div>
+		</div>
 	</div>
 
 
@@ -50,8 +50,8 @@
 				humidityTimes: [], //湿度时间
 				shipId: 1, //船舶id
 				shipArray: [], //所有船舶
-        deptCount: [],//部门数量
-        deptName: []//部门名称
+				deptCount: [], //部门数量
+				deptName: [] //部门名称
 			}
 		},
 		created() {
@@ -93,7 +93,7 @@
 			/* 初始数据 */
 			this.getShipTemperature(this.shipId);
 			this.getShipHumidity(this.shipId);
-      this.getDeptCount(this.shipId)
+			this.getDeptCount(this.shipId)
 
 		},
 		methods: {
@@ -175,94 +175,94 @@
 					});
 				})
 			},
-      /* 获取部门数量 */
-      getDeptCount(shipId) {
-        console.log("获得部门执行")
-        /* 获得数量 */
-        this.axios({
-          url: "/ship/echars/getCountsWithDepts.do",
-          method: "GET",
-          params: {
-            shipId: this.shipId
-          }
-        }).then((result) => {
-          this.deptName = [];
-          this.deptCount = [];
-          /* 数据绑定 */
-          let deptArray = result.data.data;
-          //数据用于实现线图
-          Object.keys(deptArray).forEach(key => {
-            this.deptName.push(key)
-            this.deptCount.push(deptArray[key])
-          });
-          console.log(this.deptName);
-          console.log(this.deptCount);
+			/* 获取部门数量 */
+			getDeptCount(shipId) {
+				console.log("获得部门执行")
+				/* 获得数量 */
+				this.axios({
+					url: "/ship/echars/getCountsWithDepts.do",
+					method: "GET",
+					params: {
+						shipId: this.shipId
+					}
+				}).then((result) => {
+					this.deptName = [];
+					this.deptCount = [];
+					/* 数据绑定 */
+					let deptArray = result.data.data;
+					//数据用于实现线图
+					Object.keys(deptArray).forEach(key => {
+						this.deptName.push(key)
+						this.deptCount.push(deptArray[key])
+					});
+					console.log(this.deptName);
+					console.log(this.deptCount);
 
-          // 创建一个新数组，用于ECharts的数据绑定，用于实现饼图
-          let chartData = [];
+					// 创建一个新数组，用于ECharts的数据绑定，用于实现饼图
+					let chartData = [];
 
-          // 遍历deptArray对象，将键和值转换为ECharts需要的格式
-          Object.keys(deptArray).forEach(key => {
-            chartData.push({
-              name: key,
-              value: deptArray[key]
-            });
-          });
+					// 遍历deptArray对象，将键和值转换为ECharts需要的格式
+					Object.keys(deptArray).forEach(key => {
+						chartData.push({
+							name: key,
+							value: deptArray[key]
+						});
+					});
 
-          var myChart = echarts.init(this.$refs.echartsContainer4);
-          myChart.setOption({
-            //线图的代码
-            /*
-            title: {
-              text: '部门人数'
-            },
-            xAxis: {
-              type: 'category',
-              data: this.deptName
-            },
-            yAxis: {
-              type: 'value'
-            },
-            series: [{
-              data: this.deptCount,
-              type: 'line',
-              smooth: true
-            }]*/
-
-            //饼图的代码
-            tooltip: {
-              trigger: 'item'
-            },
-            legend: {
-              top: '5%',
-              left: 'center'
-            },
-            series: [
-              {
-                name: this.shipArray[shipId].name,
-                type: 'pie',
-                radius: ['40%', '70%'],
-                avoidLabelOverlap: false,
-                label: {
-                  show: false,
-                  position: 'center'
-                },
-                emphasis: {
-                  label: {
-                    show: true,
-                    fontSize: 40,
-                    fontWeight: 'bold'
-                  }
-                },
-                labelLine: {
-                  show: false
-                },
-                data: chartData
-              }
-            ]
-          });
-        })
-      }
+					var myChart = echarts.init(this.$refs.echartsContainer4);
+					myChart.setOption({
+						//线图的代码
+						/*
+						title: {
+						  text: '部门人数'
+						},
+						xAxis: {
+						  type: 'category',
+						  data: this.deptName
+						},
+						yAxis: {
+						  type: 'value'
+						},
+						series: [{
+						  data: this.deptCount,
+						  type: 'line',
+						  smooth: true
+						}]*/
+						title: {
+							text: '部门人数'
+						},
+						//饼图的代码
+						tooltip: {
+							trigger: 'item'
+						},
+						legend: {
+							top: '5%',
+							left: 'right'
+						},
+						series: [{
+							name: this.shipArray[shipId - 1].name,
+							type: 'pie',
+							radius: ['40%', '70%'],
+							avoidLabelOverlap: false,
+							label: {
+								show: false,
+								position: 'center'
+							},
+							emphasis: {
+								label: {
+									show: true,
+									fontSize: 40,
+									fontWeight: 'bold'
+								}
+							},
+							labelLine: {
+								show: false
+							},
+							data: chartData
+						}]
+					});
+				})
+			}
 		}
 	}
 </script>
@@ -284,11 +284,11 @@
 		top: 0px;
 	}
 
-  #box4 {
-    position: relative;
-    left: 0px;
-    top: 0px;
-  }
+	#box4 {
+		position: relative;
+		left: 0px;
+		top: 0px;
+	}
 
 	#echartsContainer2 {
 		position: absolute;
@@ -306,11 +306,11 @@
 		z-index: 99999;
 	}
 
-  #echartsContainer4 {
-    position: absolute;
-    left: 100px;
-    top: -5px;
-    /* 元素置顶 */
-    z-index: 99999;
-  }
+	#echartsContainer4 {
+		position: absolute;
+		left: 100px;
+		top: -5px;
+		/* 元素置顶 */
+		z-index: 99999;
+	}
 </style>
