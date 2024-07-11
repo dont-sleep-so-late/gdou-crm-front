11e6951dc608ec8947f50b78bbd7377a18058d7a
dont-sleep-so-late <Imtiancai233@qq.com>
2024-07-11 11:18:31 +0800

    feat: 重构页面框架

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,202 +1,184 @@
 <template>
 	<div id="box">
-		<div class="layui-layout-admin">
-			<div class="layui-header">
-				<!--平台名称-->
-				<div class="layui-logo">后台管理平台</div>
-				<!--左侧主菜单-->
-				<ul class="layui-nav layui-layout-left">
-					<li class="layui-nav-item"><a href="#">日志设置</a></li>
-					<li class="layui-nav-item"><a href="#">系统设置</a></li>
-					<li class="layui-nav-item"><a href="#">统计</a></li>
-					<li class="layui-nav-item"><a href="#">帮助</a></li>
-				</ul>
-				<!--右侧主菜单-->
-				<ul class="layui-nav layui-layout-right">
-					<li class="layui-nav-item">
-						<a href="javascript:;">
-							<span id="username">{{username}}</span>
-							<img id="headImg" style="width: 40px;height: 40px"
-								:src="'/ship/download/downloadImg.do?imgUrl='+imgUrl">
-						</a>
-						<dl class="layui-nav-child">
-							<dd><a href="#" @click="logout()">退出</a></dd>
-						</dl>
-					</li>
-				</ul>
-			</div>
-			<div class="layui-side layui-bg-black">
-				<ul class="layui-nav layui-nav-tree" lay-filter="menu">
-					<li class="layui-nav-item">
-						<a href="#">
-							<i class="layui-icon layui-icon-camera"></i>账号管理
-						</a>
-						<dl class="layui-nav-child">
-							<dd>
-								<a href="javascript:void(0)" id="1" @click="getAccounts()">
-									<i class="layui-icon layui-icon-user"></i>账号列表
-								</a>
-							</dd>
-							<dd>
-								<a href="javascript:void(0)" id="2" @click="editHeadImg()">
-									<i class="layui-icon layui-icon-upload"></i>修改头像
-								</a>
-							</dd>
-							<dd>
-								<a href="javascript:void(0)" id="3" @click="editPwd()">
-									<i class="layui-icon layui-icon-password"></i>修改密码
-								</a>
-							</dd>
-						</dl>
-					</li>
-					<li class="layui-nav-item">
-						<a href="javascript:void(0)" id="4" @click="getUsers()">
-							<i class="layui-icon layui-icon-user"></i>职员管理
-						</a>
-					</li>
-					<li class="layui-nav-item">
-						<a href="javascript:void(0)" id="5" @click="getDepts()">
-							<i class="layui-icon layui-icon-location"></i>部门管理
-						</a>
-					</li>
-					<li class="layui-nav-item">
-						<a href="javascript:void(0)" id="6" @click="getSearchs()">
-							<i class="layui-icon layui-icon-theme"></i>搜索管理
-						</a>
-					</li>
-					<li class="layui-nav-item">
-						<a href="javascript:void(0)" id="7" @click="getShips()">
-							<i class="layui-icon layui-icon-email"></i>船舶管理
-						</a>
-					</li>
-					<li class="layui-nav-item">
-						<a href="javascript:void(0)" id="8" @click="echars()">
-							<i class="layui-icon layui-icon-email"></i>报表管理
-						</a>
-					</li>
-				</ul>
-			</div>
+		<el-container>
 
-			<div id="box">
-				<router-view></router-view>
-			</div>
-			<!-- <div class="layui-body">
-		        <div class="layui-tab" lay-allowClose="true" lay-filter="zst">
-		            <ul class="layui-tab-title">
-		                <li class="layui-this">首页</li>
-		            </ul>
-		            <div class="layui-tab-content">
-		                <div id="welcome" class="layui-tab-item layui-show"></div>
-		            </div>
-		        </div>
-		    </div> -->
-			<!-- <div class="layui-footer">
-				<center>
-					2022-9999&copy;版权所有|维权必究
-				</center>
-			</div> -->
-		</div>
+			<el-container>
+				<el-header>
+					<!--平台名称-->
+					<div>
+						<h2>船舶后台管理平台</h2>
+					</div>
+					<!--右侧主菜单-->
+					<el-dropdown>
+						<a href="javascript:;">
+							<span class="username">{{ username }}</span>
+							<img id="headImg" style="width: 40px;height: 40px"
+								:src="'/ship/download/downloadImg.do?imgUrl=' + imgUrl">
+						</a>
+						<el-dropdown-menu slot="dropdown">
+							<el-dropdown-item @click.native="logout()">退出登录</el-dropdown-item>
+						</el-dropdown-menu>
+					</el-dropdown>
+				</el-header>
+				<el-container>
+					<el-aside width="200px">
+						<el-menu default-active="1" class="el-menu-vertical-demo">
+							<el-submenu index="1">
+								<template slot="title">
+									<i class="el-icon-location"></i>
+									<span>账号管理</span>
+								</template>
+								<el-menu-item-group>
+									<el-menu-item index="getAccounts" @click="getAccounts()" id="1">账号列表</el-menu-item>
+									<el-menu-item index="editHeadImg" @click="editHeadImg()" id="2">修改头像</el-menu-item>
+									<el-menu-item index="editPwd" @click="editPwd()" id="3">修改密码</el-menu-item>
+								</el-menu-item-group>
+							</el-submenu>
+							<el-menu-item index="getUsers" id="4" @click="getUsers()">
+								<i class="el-icon-menu"></i>
+								<span slot="title">职员管理</span>
+							</el-menu-item>
+							<el-menu-item index="getDepts" id="5" @click="getDepts()">
+								<i class="el-icon-document"></i>
+								<span slot="title">部门管理</span>
+							</el-menu-item>
+							<el-menu-item index="getSearchs" id="6" @click="getSearchs()">
+								<i class="el-icon-setting"></i>
+								<span slot="title">搜索管理</span>
+							</el-menu-item>
+							<el-menu-item index="getShips" id="7" @click="getShips()">
+								<i class="el-icon-setting"></i>
+								<span slot="title">船舶管理</span>
+							</el-menu-item>
+							<el-menu-item index="echars" id="8" @click="echars()">
+								<i class="el-icon-setting"></i>
+								<span slot="title">报表管理</span>
+							</el-menu-item>
+						</el-menu>
+					</el-aside>
+					<el-main>
+						<div id="box">
+							<router-view></router-view>
+						</div>
+					</el-main>
+				</el-container>
+
+			</el-container>
+		</el-container>
+
 	</div>
 </template>
 
 <script>
-	export default {
-		name: "",
-		props: ["imgUrl", "username"],
-		data() {
-			return {
-				// imgUrl:imgUrl,
-				// username:username
-			}
+export default {
+	name: "",
+	props: ["imgUrl", "username"],
+	data() {
+		return {
+			// imgUrl:imgUrl,
+			// username:username
+		}
+	},
+	/* created(){
+		let userJson = window.localStorage.getItem("user");
+		  let user = JSON.parse(userJson);
+		this.imgUrl=user.imgUrl
+		this.username=user.username
+	}, */
+	methods: {
+		logout() {
+			this.axios({
+				url: "/ship/account/logout.do",
+				method: "POST",
+				params: {}
+			}).then((resp) => {
+				this.$emit("log-out")
+			})
 		},
-		/* created(){
-			let userJson = window.localStorage.getItem("user");
-		 	let user = JSON.parse(userJson);
-			this.imgUrl=user.imgUrl
-			this.username=user.username
-		}, */
-		methods: {
-			logout() {
-				this.axios({
-					url: "/ship/account/logout.do",
-					method: "POST",
-					params: {}
-				}).then((resp) => {
-					this.$emit("log-out")
-				})
-			},
-			getAccounts() {
-				/* 路由 */
-				this.$router.push({
-					path: "/getAccounts",
-					query: {
-						rand: Math.random()
-					}
-				})
-			},
-			editHeadImg() {
-				this.$router.push({
-					path: "/editHeadImg",
-					query: {
-						rand: Math.random()
-					}
-				})
-			},
-			editPwd() {
-				this.$router.push({
-					path: "/editPwd",
-					query: {
-						rand: Math.random()
-					}
-				})
-			},
-			getUsers() {
-				this.$router.push({
-					path: "/getUsers",
-					query: {
-						rand: Math.random()
-					}
-				})
-			},
-			getSearchs() {
-				this.$router.push({
-					path: "/getSearchs",
-					query: {
-						rand: Math.random()
-					}
-				})
-			},
-			getDepts() {
-				this.$router.push({
-					path: "/getDepts",
-					query: {
-						rand: Math.random()
-					}
-				})
-			},
-			getShips() {
-				this.$router.push({
-					path: "/getShips",
-					query: {
-						rand: Math.random()
-					}
-				})
-			},
-			echars() {
-				this.$router.push({
-					path: "/echars",
-					query: {
-						rand: Math.random()
-					}
-				})
-			}
+		getAccounts() {
+			/* 路由 */
+			this.$router.push({
+				path: "/getAccounts",
+				query: {
+					rand: Math.random()
+				}
+			})
+		},
+		editHeadImg() {
+			this.$router.push({
+				path: "/editHeadImg",
+				query: {
+					rand: Math.random()
+				}
+			})
+		},
+		editPwd() {
+			this.$router.push({
+				path: "/editPwd",
+				query: {
+					rand: Math.random()
+				}
+			})
+		},
+		getUsers() {
+			this.$router.push({
+				path: "/getUsers",
+				query: {
+					rand: Math.random()
+				}
+			})
+		},
+		getSearchs() {
+			this.$router.push({
+				path: "/getSearchs",
+				query: {
+					rand: Math.random()
+				}
+			})
+		},
+		getDepts() {
+			this.$router.push({
+				path: "/getDepts",
+				query: {
+					rand: Math.random()
+				}
+			})
+		},
+		getShips() {
+			this.$router.push({
+				path: "/getShips",
+				query: {
+					rand: Math.random()
+				}
+			})
+		},
+		echars() {
+			this.$router.push({
+				path: "/echars",
+				query: {
+					rand: Math.random()
+				}
+			})
 		}
 	}
+}
 </script>
 
 <style scoped>
-	#box {
-		padding-top: 50px;
-		padding-left: 120px;
-	}
+.el-header {
+	background-color: #0080ff;
+	color: #ffffff;
+	line-height: 60px;
+	font-weight: 600;
+	display: flex;
+	justify-content: space-between;
+}
+
+.username {
+	color: #fff;
+}
+
+.el-icon-arrow-down {
+	font-size: 12px;
+}
 </style>
